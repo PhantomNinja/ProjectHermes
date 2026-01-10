00208e416d3b0e78ab2c399f98c0ded91397a807
PhantomNinja <train.trooper@gmail.com>
2026-01-09 20:00:25 -0600

    Added Airdash
    Currently not working with double tapping A or D, CTRL is the temporary keybind to trigger it
    Moves the player forward in their direction of travel, 1 unit of dash speed translates to 1 unit traveled in game unless going left which is 1 unit less

---
+++ Assets/Scripts/AirDash.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class AirDash : MonoBehaviour
+{
+    CharacterController characterController;
+
+    [Header("Dash Values")]
+    [SerializeField] float dashSpeed;
+    
+    [Header("Input Actions")]
+    public InputAction dashAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        dashAction = InputSystem.actions.FindAction("AirDash");
+        characterController = GetComponent<CharacterController>();
+        
+        // Currently every 10 units of speed translates to approximately 1 unit traveled in game
+        dashSpeed = dashSpeed * 10; 
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (dashAction.WasCompletedThisFrame())
+        {
+            Debug.Log("Player Dashed");
+            characterController.SimpleMove(new Vector3(dashSpeed, 0, 0));
+        }
+    }
+}
